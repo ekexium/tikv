7d863015551bc5bb0e240b50c7c7a23674460d84
ekexium <ekexium@gmail.com>
2022-05-26 10:34:14 +0800

    return NotInRange instead of Other
    Signed-off-by: ekexium <ekexium@gmail.com>

--- components/raftstore/src/store/region_snapshot.rs
+++ components/raftstore/src/store/region_snapshot.rs
@@ -207,8 +207,7 @@ where
             self.region.get_id(),
             self.region.get_start_key(),
             self.region.get_end_key(),
-        )
-        .map_err(|e| EngineError::Other(box_err!(e)))?;
+        )?;
         let data_key = keys::data_key(key);
         self.snap
             .get_value_opt(opts, &data_key)
@@ -226,8 +225,7 @@ where
             self.region.get_id(),
             self.region.get_start_key(),
             self.region.get_end_key(),
-        )
-        .map_err(|e| EngineError::Other(box_err!(e)))?;
+        )?;
         let data_key = keys::data_key(key);
         self.snap
             .get_value_cf_opt(opts, cf, &data_key)
